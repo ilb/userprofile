5b7e5af6bf9ccfe6c4b63893349727a12e103adc
puphs <k.x3@mail.ru>
2021-02-20 08:27:59 -0800

    replace AutoForm with Form

--- pages/salepointSelect.js
+++ pages/salepointSelect.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Header, Form } from 'semantic-ui-react';
 import { AutoForm } from 'uniforms-semantic';
-import { createSchemaBridge, CustomAutoField } from '@ilb/uniformscomponents';
+import { createSchemaBridge } from '@ilb/uniformscomponents';
 import { withRouter } from 'next/router';
 import { useForm } from 'react-hook-form';
 import { processUsecase } from '../libs/usecases';
 
-// function FormSelect({ name, id, label, defaultValue, register, required, options, errors }) {
-//   return (
-//     <Form.Field required={required || false}>
-//       <label htmlFor={id || name}>{label}</label>
-//       <select name={id || name} return={register} defaultValue={defaultValue}>
-//         {options.map(({ value, label }) => (
-//           <option key={value} value={value}>
-//             {label}
-//           </option>
-//         ))}
-//       </select>
-//       {/* {errors[name] && <FieldError error={errors[name].message} />} */}
-//     </Form.Field>
-//   );
-// }
+function FormSelect({ name, id, label, defaultValue, register, required, options, errors }) {
+  return (
+    <Form.Field required={required || false}>
+      <label htmlFor={id || name}>{label}</label>
+      <select name={id || name} ref={register} defaultValue={defaultValue}>
+        {options.map(({ value, label }) => (
+          <option key={value} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+      {/* {errors[name] && <FieldError error={errors[name].message} />} */}
+    </Form.Field>
+  );
+}
 
 function SalepointSelectPage({ router, request, response, schema }) {
   const allSalepoints = [response.currentSalepoint, ...response.salepoints];
@@ -34,8 +34,8 @@ function SalepointSelectPage({ router, request, response, schema }) {
 
   return (
     <Container>
-      {/* <Form onSubmit={handleSubmit(onSubmit)}>
-         <FormSelect
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <FormSelect
           name="salepoint"
           label="Точка продаж"
           options={allSalepoints.map(({ code, name }) => ({
@@ -45,15 +45,15 @@ function SalepointSelectPage({ router, request, response, schema }) {
           register={register}
         />
         <Form.Button type="submit">Submit</Form.Button>
-      </Form> */}
-      <AutoForm
+      </Form>
+      {/* <AutoForm
         schema={createSchemaBridge(schema)}
         model={request}
         autoField={CustomAutoField}
         onSubmit={onSubmit}
         showInlineError={true}
       />
-      {response.greeting && <Header as="h1">{response.greeting}</Header>}
+      {response.greeting && <Header as="h1">{response.greeting}</Header>} */}
     </Container>
   );
 }
