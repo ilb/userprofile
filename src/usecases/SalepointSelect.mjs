15f735d017de750daf812a3825960f7e7065e59f
puphs <k.x3@mail.ru>
2021-02-20 00:18:17 -0800

    add retrieving user salepoints in SalepointSelect

--- src/usecases/SalepointSelect.mjs
+++ src/usecases/SalepointSelect.mjs
@@ -2,9 +2,12 @@
  * Search use case
  */
 export default class SalepointSelect {
-  constructor({ salepointProvider }) {
+  constructor({ salepointProvider, userRepository, userSalepointRepository, salepointRepository }) {
     this.salepointProvider = salepointProvider;
-    // this.currentUser = currentUser;
+    this.userRepository = userRepository;
+    this.userSalepointRepository = userSalepointRepository;
+    this.salepointRepository = salepointRepository;
+    this.currentUser = this.currentUser = process.env.USER;
   }
   /**
    * process use case
@@ -12,22 +15,28 @@ export default class SalepointSelect {
    */
   async process(request) {
     const result = {};
-    if (request.q) {
-      // result.rows = await this.dictionaryRepository.search(request.q);
-    }
+    const salepoints = await this.salepointProvider.getSalepoints(this.currentUser);
+
+    const user = await this.userRepository.findByCode(this.currentUser);
+    const userSalepoints = await this.userSalepointRepository.findByUserId(user.id);
+    const currentUserSalepoint = userSalepoints.find((userSalepoint) => !userSalepoint.endDate);
+    const currentSalepoint = await this.salepointRepository.findById(
+      currentUserSalepoint.salepointId
+    );
+    const { name, code } = currentSalepoint;
+    result.currentSalepoint = { name, code };
+    result.salepoints = salepoints
+      .filter(({ code }) => code !== currentSalepoint.code)
+      .map(({ name, code }) => ({ name, code }));
+    // if (request.q) {
+    //   result.rows = await this.dictionaryRepository.search(request.q);
+    // }
     return result;
   }
 
   /*eslint no-unused-vars: ["error", { "args": "none" }]*/
   async getSchema(request) {
-    const salepoints = await this.salepointProvider.getSalepoints(this.currentUser);
-    const schema = {
-      type: 'object',
-      properties: {
-        category: { title: 'Точка продаж', type: 'string', enum: salepoints.map((sp) => sp.name) }
-      },
-      required: ['salepointUid']
-    };
+    const schema = {};
     return schema;
   }
 }
