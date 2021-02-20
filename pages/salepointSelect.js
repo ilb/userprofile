721fd2cfc4d73cc2e50a98b9cee475f753d52f54
puphs <k.x3@mail.ru>
2021-02-20 00:20:18 -0800

    add showing current salepoint in salepointSelect

--- pages/salepointSelect.js
+++ pages/salepointSelect.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import { Container, Header } from 'semantic-ui-react';
-import { AutoForm } from 'uniforms-semantic';
-import { createSchemaBridge } from '../libs/uniforms';
 import { withRouter } from 'next/router';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Container, Form } from 'semantic-ui-react';
 import { processUsecase } from '../libs/usecases';
 
+function FormSelect({ name, id, label, defaultValue, register, required, options, errors }) {
+  return (
+    <Form.Field required={required || false}>
+      <label htmlFor={id || name}>{label}</label>
+      <select name={id || name} ref={register} defaultValue={defaultValue}>
+        {options.map(({ value, label }) => (
+          <option key={value} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+      {/* {errors[name] && <FieldError error={errors[name].message} />} */}
+    </Form.Field>
+  );
+}
+
 function SalepointSelectPage({ router, request, response, schema }) {
-  console.log({ request });
+  const allSalepoints = [response.currentSalepoint, ...response.salepoints];
+
+  const { register, handleSubmit } = useForm();
 
   function onSubmit(query) {
-    router.push({ pathname: 'autoform', query });
+    console.log(query);
+    // router.push({ query });
   }
 
   return (
     <Container>
-      <AutoForm
-        schema={createSchemaBridge(schema)}
-        model={request}
-        onSubmit={onSubmit}
-        showInlineError={true}
-      />
-      {response.greeting && <Header as="h1">{response.greeting}</Header>}
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <FormSelect
+          name="salepoint"
+          label="Точка продаж"
+          options={allSalepoints.map(({ code, name }) => ({
+            value: code,
+            label: name
+          }))}
+          register={register}
+        />
+        <Form.Button type="submit">Submit</Form.Button>
+      </Form>
     </Container>
   );
 }
