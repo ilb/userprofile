f6ee0ff605a30153348a6072da6585c1d065e417
puphs <k.x3@mail.ru>
2021-02-25 15:40:42 +0400

    create SalepointsHistory usecase

---
+++ src/usecases/SalepointsHistory.mjs
@@ -0,0 +1,60 @@
+const HISTORY_RECORDS_LIMIT = 10;
+
+export default class SalepointHistory {
+  constructor({ userRepository, salepointService }) {
+    this.userRepository = userRepository;
+    this.salepointService = salepointService;
+  }
+
+  async process(req) {
+    const { userCode, begDate, endDate, page } = req;
+    if (!(userCode && begDate && endDate)) {
+      return null;
+    }
+    const skippedRecordsCount = (page - 1) * HISTORY_RECORDS_LIMIT || 0;
+
+    const user = await this.userRepository.findByCode(userCode);
+    const begDateIsoString = new Date(begDate).toISOString();
+    const endDateIsoString = new Date(endDate).toISOString();
+    const salepointsHistorySize = await this.salepointService.getSalepointsHistorySize(
+      user,
+      begDateIsoString,
+      endDateIsoString
+    );
+    const salepointsHistory = await this.salepointService.getSalepointsHistory(
+      user,
+      begDateIsoString,
+      endDateIsoString,
+      skippedRecordsCount,
+      HISTORY_RECORDS_LIMIT
+    );
+    return { userName: user.name, salepointsHistory, salepointsHistorySize };
+  }
+
+  async schema() {
+    const users = await this.userRepository.getAll();
+
+    const schema = {
+      type: 'object',
+      properties: {
+        userCode: {
+          title: 'Пользователь',
+          type: 'string',
+          options: users.map(({ code, name }) => ({ value: code, label: name }))
+        },
+        begDate: {
+          title: 'Начало периода',
+          type: 'string',
+          format: 'date'
+        },
+        endDate: {
+          title: 'Конец периода',
+          type: 'string',
+          format: 'date'
+        }
+      },
+      required: ['userCode', 'begDate', 'endDate']
+    };
+    return schema;
+  }
+}
