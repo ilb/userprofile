1386d0136573d2c14d469f37c95216d51e240416
danil <puphs.dev@gmail.com>
2021-02-27 12:38:08 +0400

    extract validation logic to schemaValidation module

--- libs/usecases/index.mjs
+++ libs/usecases/index.mjs
@@ -1,55 +1,27 @@
 import application from '../application.mjs';
-import ajv from '../utils/ajv.mjs';
+import { ValidationError, validateBySchema } from '../utils/schemaValidation.mjs';
 import Response from '../utils/Response.mjs';
-
-class ValidationError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'ValidationError';
-  }
-}
-
-function validateUsecase(request, schema) {
-  const validate = ajv.compile(schema);
-  if (validate(request)) {
-    return;
-  } else {
-    for (const err of validate.errors) {
-      const errProperty = (function () {
-        const endpoints = err.dataPath.split('/');
-        return endpoints[endpoints.length - 1];
-      })();
-
-      switch (err.keyword) {
-        case 'required':
-          throw new ValidationError(`В запросе отсутствует ${err.params.missingProperty}`);
-        case 'type':
-          throw new ValidationError(`Тип ${errProperty} должен быть ${err.params.type}`);
-        default:
-          throw new ValidationError(`Ошибка при валидации данных`);
-      }
-    }
-  }
-}
 
 export async function processUsecase({ query: request, req }, useCase) {
   const scope = await application.createScope(req);
   const usecase = scope.resolve(useCase);
   const schema = await usecase.schema(request);
   let response = null;
+  let error = null;
 
   if (Object.values(request).length) {
     try {
-      validateUsecase(request, schema);
+      validateBySchema(request, schema);
       response = await usecase.process(request);
     } catch (err) {
       response = await usecase.process({});
+      error = err.message;
     }
   } else {
     response = await usecase.process({});
   }
 
-  return { props: { request, response, schema } };
+  return { props: { request, response, schema, error } };
 }
 
 export async function processUsecaseApi(req, useCase) {
@@ -58,7 +30,7 @@ export async function processUsecaseApi(req, useCase) {
   const schema = await usecase.schema(req);
 
   try {
-    validateUsecase(req, schema);
+    validateBySchema(req, schema);
     const result = usecase.process(req);
     if (result) {
       return Response.ok(result);
