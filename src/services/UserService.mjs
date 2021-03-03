96627417c3ec41b7efb58e09d20568c787651d44
Danil <k.x3@mail.ru>
2021-03-03 13:19:12 +0400

    create user service

---
+++ src/services/UserService.mjs
@@ -0,0 +1,26 @@
+export default class UserService {
+  constructor({ userRepository, currentUser }) {
+    this.userRepository = userRepository;
+    this.currentUser = currentUser;
+  }
+
+  async getUser(code) {
+    return this.userRepository.findByCode(code);
+  }
+
+  async createUser(code) {
+    return this.userRepository.create(code);
+  }
+
+  async getOrCreateUser(code) {
+    let user = await this.getUser(code);
+    if (!user) {
+      user = await this.createUser(code);
+    }
+    return user;
+  }
+
+  async getOrCreateCurrentUser() {
+    return this.getOrCreateUser(this.currentUser);
+  }
+}
