098892abda8b2245a6771e86552e04e09384b00a
Danil <k.x3@mail.ru>
2021-03-04 15:02:23 +0400

    remove api response wrapper

--- pages/api/apiHelper.js
+++ pages/api/apiHelper.js
@@ -1,6 +1,13 @@
 import { processUsecaseApi } from '../../libs/usecases/index.mjs';
 
 export async function processApi(req, res, useCase) {
-  const { httpCode, ...restResponse } = await processUsecaseApi(req, useCase);
-  res.status(httpCode).json(restResponse);
+  const { httpCode, data } = await processUsecaseApi(req, useCase);
+  res.statusCode = httpCode;
+
+  let contentType = 'application/json';
+  if (typeof data === 'string') {
+    contentType = 'text/plain';
+  }
+  res.setHeader('Content-Type', contentType);
+  res.status(httpCode).send(data);
 }
